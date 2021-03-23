c03e2267acebbff5cbcb61d14dbf0be80819df35
Aaron <50504602+xAaronWx@users.noreply.github.com>
2021-03-23 18:23:36 -0400

    added propsEx

---
+++ red-badge-demo-ts/src/components/PropsExample.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class PropsExample extends Component {
+  render() {
+    return <div>Hello from PropsExample</div>;
+  }
+}
+
+export default PropsExample;
