0d22bcc50bf24334e0364da9b1049196359c6fbe
Rob Vanarsdall <rvanar@gmail.com>
2021-10-10 17:24:27 -0400

    refactored

--- red-badge-demo-ts/src/components/FunctionFetchOnload.jsx
+++ red-badge-demo-ts/src/components/FunctionFetchOnload.jsx
@@ -4,41 +4,39 @@ import { Container, Media } from "reactstrap";
 const FunctionFetchOnload = (props) => {
   const [blogPostData, setBlogPostData] = useState([]);
 
-  function fetchBlogData() {
+  const fetchBlogData = async () => {
     const url = "https://jsonplaceholder.typicode.com/comments?postId=1";
-    fetch(url)
-      .then((res) => res.json())
-      .then((json) => setBlogPostData(json));
-  }
-  function displayBlogPosts() {
-    return blogPostData.map((data) => (
-      <Media>
-        <Media left href="#">
-          <Media
-            object
-            src="https://picsum.photos/60"
-            alt="Generic placeholder image"
-            style={{ marginRight: "10px" }}
-          />
-        </Media>
-        <Media body>
-          <Media heading>{data.name.toUpperCase()}</Media>
-          {data.body}.
-          <p>
-            Email: <a href={`mailto:${data.email}`}>{data.email}</a>
-          </p>
-        </Media>
-      </Media>
-    ));
-  }
+    const response = await fetch(url);
+    const jsonResponse = await response.json();
+    setBlogPostData(jsonResponse);
+  };
 
   useEffect(() => {
     fetchBlogData();
   }, []);
+
   return (
     <Container>
       <h3>From the Function</h3>
-      {displayBlogPosts()}
+      {blogPostData?.map((data) => (
+        <Media>
+          <Media left href="#">
+            <Media
+              object
+              src="https://picsum.photos/60"
+              alt="Generic placeholder image"
+              style={{ marginRight: "10px" }}
+            />
+          </Media>
+          <Media body>
+            <Media heading>{data.name.toUpperCase()}</Media>
+            {data.body}.
+            <p>
+              Email: <a href={`mailto:${data.email}`}>{data.email}</a>
+            </p>
+          </Media>
+        </Media>
+      ))}
     </Container>
   );
 };
